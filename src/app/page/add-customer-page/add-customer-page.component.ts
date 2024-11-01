6e7b1eb6cdd3e71212a7a1786ea5398e4a096aeb
icet-sharadha <icet.sharadha@gmail.com>
2024-11-01 14:41:12 +0530

    add customer

--- src/app/page/add-customer-page/add-customer-page.component.ts
+++ src/app/page/add-customer-page/add-customer-page.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-customer-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './add-customer-page.component.html',
   styleUrl: './add-customer-page.component.css'
 })
 export class AddCustomerPageComponent {
+
+  public customer:any={
+    name:"",
+    address:"",
+    email:""
+  };
+
+  constructor(private http:HttpClient){}
+
+  public addCustomer(){
+    this.http.post("http://localhost:8080/customer/add-customer",this.customer).subscribe((data)=>{
+        alert("Customer Added!!!!");
+    })
+  }
+
 
 }
