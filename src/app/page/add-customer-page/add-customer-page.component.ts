1e6f4b0f135e37050c9f165428b14895e7611290
icet-sharadha <icet.sharadha@gmail.com>
2024-11-01 12:09:17 +0530

    create compornents

---
+++ src/app/page/add-customer-page/add-customer-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-customer-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-customer-page.component.html',
+  styleUrl: './add-customer-page.component.css'
+})
+export class AddCustomerPageComponent {
+
+}
---
+++ src/app/page/manage-customer-page/manage-customer-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-customer-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-customer-page.component.html',
+  styleUrl: './manage-customer-page.component.css'
+})
+export class ManageCustomerPageComponent {
+
+}
